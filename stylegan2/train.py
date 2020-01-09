7fa56a14020208b309a28186e86f9647418d9ee7
Allen Ng <lukexng@gmail.com>
2020-01-09 16:20:13 +0800

    add log to loss

--- stylegan2/train.py
+++ stylegan2/train.py
@@ -459,11 +459,13 @@ class Trainer:
         mul /= self.world_size or 1
         if mul != 1:
             loss *= mul
+        loss_start = time.time()
         if self.half:
             with amp.scale_loss(loss, opt) as scaled_loss:
                 scaled_loss.backward()
         else:
             loss.backward()
+        print("--- loss took %s seconds ---\n" % (time.time() - loss_start))
         return loss * (self.world_size or 1)
 
     def train(self, iterations, callbacks=None, verbose=True):
